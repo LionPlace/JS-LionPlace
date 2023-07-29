48901ac11dd4cc001bd6dee6970f8843e0bd5495
whddbsl <whddbs04140@naver.com>
2023-07-30 00:48:07 +0900

    📝 Feat: 카카오 지도 API 랜더링 코드 작성

--- client/js/map/map.js
+++ client/js/map/map.js
@@ -1,7 +1,70 @@
-const container = document.getElementById('map');
-const options = {
-  center: new kakao.maps.LatLng(33.450701, 126.570667),
-  level: 3,
+const getStorInfo = async options => {
+  const defaultOptions = {
+    method: 'GET',
+    body: null,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+  };
+  const {url, ...restOptions} = {
+    ...defaultOptions,
+    ...options,
+    headers: {
+      ...defaultOptions.headers,
+      ...options.headers,
+    },
+  };
+  const response = await fetch(url, restOptions);
+
+  if (response.ok) {
+    response.data = await response.json();
+  }
+
+  return response;
 };
 
-const map = new kakao.maps.Map(container, options);
+function renderOverlay(map, storeData) {
+  for (let i = 0; i < storeData.length; i += 1) {
+    const content = `<div style="margin-bottom: 110px; background: green; border-radius: 10px; padding-left:10px; padding-right:10px">${storeData[i].title}</div>`;
+
+    const customOverlay = new kakao.maps.CustomOverlay({
+      position: new kakao.maps.LatLng(storeData[i].lat, storeData[i].lng),
+      content,
+    });
+    customOverlay.setMap(map);
+  }
+}
+
+function setBounds(map, storeData) {
+  const bounds = new kakao.maps.LatLngBounds();
+  for (let i = 0; i < storeData.length; i += 1) {
+    const position = new kakao.maps.LatLng(storeData[i].lat, storeData[i].lng);
+    const marker = new kakao.maps.Marker({
+      position,
+      title: storeData[i].title,
+    });
+    marker.setMap(map);
+    bounds.extend(position);
+  }
+  map.setBounds(bounds);
+}
+
+function renderMap(storeData) {
+  const container = document.getElementById('map');
+  const options = {
+    center: new kakao.maps.LatLng(storeData[0].lat, storeData[0].lng),
+    level: 3,
+  };
+  const map = new kakao.maps.Map(container, options);
+
+  setBounds(map, storeData);
+  renderOverlay(map, storeData);
+}
+
+(async function render() {
+  const URL = 'http://localhost:3000/info';
+  const response = await getStorInfo({url: URL});
+  const storeData = response.data;
+  renderMap(storeData);
+})();
