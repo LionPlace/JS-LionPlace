217202d5d9c8f756fc116d1153c234e904281ccd
whddbsl <whddbs04140@naver.com>
2023-07-30 19:08:44 +0900

    📝 Feat: 데이터 셋 적용해서 데이터 가져오기

--- client/js/map/map.js
+++ client/js/map/map.js
@@ -1,4 +1,7 @@
-const getStorInfo = async options => {
+import {getNode} from '../../lib/index.js';
+
+// * 카카오 지도 API
+const getStoreInfo = async options => {
   const defaultOptions = {
     method: 'GET',
     body: null,
@@ -25,7 +28,12 @@ const getStorInfo = async options => {
 };
 
 function renderOverlay(map, storeData) {
-  for (let i = 0; i < storeData.length; i += 1) {
+  storeData.visited.forEach(visitedPlace => {
+    const placeName = Object.keys(visitedPlace)[0];
+    const [latitude, longitude] = [
+      visitedPlace[placeName].location.latitude,
+      visitedPlace[placeName].location.longitude,
+    ];
     const content = `<div style="margin-bottom: 110px;   
     display: inline-flex;
     font-size: 12px;
@@ -37,46 +45,79 @@ function renderOverlay(map, storeData) {
     background-color: #FFFFFF;
     padding-right: 4px;
     padding-left: 2px;">
-        <span style="margin-left:4px">${storeData[i].title}</span>
+        <span style="margin-left:4px">${placeName}</span>
     </div>`;
 
     const customOverlay = new kakao.maps.CustomOverlay({
-      position: new kakao.maps.LatLng(storeData[i].lat, storeData[i].lng),
+      position: new kakao.maps.LatLng(latitude, longitude),
       content,
     });
     customOverlay.setMap(map);
-  }
+  });
 }
 
 function setBounds(map, storeData) {
   const bounds = new kakao.maps.LatLngBounds();
-  for (let i = 0; i < storeData.length; i += 1) {
-    const position = new kakao.maps.LatLng(storeData[i].lat, storeData[i].lng);
+  storeData.visited.forEach(visitedPlace => {
+    const placeName = Object.keys(visitedPlace)[0];
+    const [latitude, longitude] = [
+      visitedPlace[placeName].location.latitude,
+      visitedPlace[placeName].location.longitude,
+    ];
+    const position = new kakao.maps.LatLng(latitude, longitude);
     const marker = new kakao.maps.Marker({
       position,
-      title: storeData[i].title,
+      title: placeName,
     });
     marker.setMap(map);
     bounds.extend(position);
-  }
+  });
   map.setBounds(bounds);
 }
 
 function renderMap(storeData) {
   const container = document.getElementById('map');
+  const centerPlace = storeData.visited[0];
+  const placeName = Object.keys(storeData.visited[0])[0];
+  const [latitude, longitude] = [
+    centerPlace[placeName].location.latitude,
+    centerPlace[placeName].location.longitude,
+  ];
   const options = {
-    center: new kakao.maps.LatLng(storeData[0].lat, storeData[0].lng),
+    center: new kakao.maps.LatLng(latitude, longitude),
     level: 3,
   };
   const map = new kakao.maps.Map(container, options);
-
   setBounds(map, storeData);
   renderOverlay(map, storeData);
 }
 
 (async function render() {
-  const URL = 'http://localhost:3000/info';
-  const response = await getStorInfo({url: URL});
+  const URL = 'http://localhost:3000/lion';
+  const response = await getStoreInfo({url: URL});
   const storeData = response.data;
   renderMap(storeData);
 })();
+
+//* 카카오 지도 API
+
+//* 지도 확장 버튼
+const expandButton = getNode('.expand-button');
+
+function test() {
+  console.log('test');
+}
+
+expandButton.addEventListener('click', test);
+//* 지도 확장 버튼
+
+//* 스크롤 내리기 버튼
+const mapButton = getNode('.map-button');
+
+function moveScrollBottom() {
+  window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'});
+}
+
+mapButton.addEventListener('click', moveScrollBottom);
+
+//* 스크롤 내리기 버튼
