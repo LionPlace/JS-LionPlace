50e3c91538b30737c00a5ed50abe7a9636a9da10
whddbsl <whddbs04140@naver.com>
2023-07-31 09:18:08 +0900

    📚 Docs: js에 작성된 테일윈드 코드 컴파일

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ['./client/**/*.html'],
+  content: ['./client/**/*.{html,js}'],
   theme: {
     extend: {
       colors: {
